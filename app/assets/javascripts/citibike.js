effe28456af99e80efe49783b7b744f17a71b63d
drewtunney <bryantunney@gmail.com>
2014-05-08 15:58:25 -0400

    fixed bounds to reset on each directions query

--- app/assets/javascripts/citibike.js
+++ app/assets/javascripts/citibike.js
@@ -1,9 +1,5 @@
 var currentLocation;
 
-App.directionsService  = new google.maps.DirectionsService();
-App.bounds = new google.maps.LatLngBounds();
-
-
 App.directionsDisplay1 = new google.maps.DirectionsRenderer({
   preserveViewport: true,
   suppressMarkers : true,
@@ -23,131 +19,6 @@ App.directionsDisplay3 = new google.maps.DirectionsRenderer({
   polylineOptions : {strokeColor:'yellow', strokeWeight: 5, strokeOpacity: 1},
 });
 
-// load stations object into window
-App.updateStationsInfo = function(){
-  $.getJSON('/stations', function(data){ 
-    App.stations = data; 
-    console.log(App.stations);
-  });
-};
-
-App.getStation = function(address, waypoint) {
-  var geocoder = new google.maps.Geocoder();
-
-  if (address === "Current Location"){
-    App.getCurrentLocation();
-
-  } else {
-    geocoder.geocode({
-        'address': address
-    }, function (results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-          var latitude  = results[0].geometry.location.lat();
-          var longitude = results[0].geometry.location.lng();
-
-          if (waypoint === "start") {
-            var station = findPickUpStation(latitude, longitude);
-            App.setStation(station, waypoint);
-          }
-          else {
-            var station = findDropOffStation(latitude, longitude);
-            App.setStation(station, waypoint);
-          }
-        } 
-        else {
-          alert("Enter a destination!");
-        }
-    });
-  } 
-}
-
-App.getCurrentLocation = function(){
-  if(navigator.geolocation) {
-    var pos;
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-      currentLocation = pos;
-      console.log(currentLocation);
-      // passing pick up station, current location || not sure where to go from here //
-      var station = findPickUpStation(currentLocation["k"], currentLocation["A"]);
-      App.setStation(station, "start", currentLocation)
-    });
-  } else {
-    handleNoGeolocation(false);
-  }
-};
-
-App.setStation = function(station, waypoint, currentLocation) {
-  App[waypoint + "Station"] = station;
-  App.buildDirections(currentLocation);
-
-}
-
-App.buildDirections = function(){
-  if (App.startStation && App.endStation) {
-    // console.log("current location in build direction ---> " + currentLocation);
-    var startStatLatLng = new google.maps.LatLng(App.startStation.latitude, App.startStation.longitude);
-    var endStatLatLng   = new google.maps.LatLng(App.endStation.latitude,   App.endStation.longitude);
-
-    App.bounds.extend(startStatLatLng);
-    App.bounds.extend(endStatLatLng);
-
-    // TODO: for current location, App.StartPoint needs to be (longitude, lattitude), not "Current Location"
-    var transitType = $('#transit-type').val()
-
-    var startLeg = {
-      origin: (currentLocation != undefined ? currentLocation : App.startPoint),
-      destination: startStatLatLng,
-      travelMode: google.maps.TravelMode.WALKING
-    };
-
-    // if the user wants to use driving directions instead of bike paths
-
-    if (transitType == "BICYCLING"){
-       var middleLeg = {
-        origin: startStatLatLng,
-        destination: endStatLatLng,
-        travelMode: google.maps.TravelMode.BICYCLING
-        }
-      }
-    else{
-      var middleLeg = {
-        origin: startStatLatLng,
-        destination: endStatLatLng,
-        travelMode: google.maps.TravelMode.DRIVING
-      };
-    }
-
-    var endLeg = {
-      origin: endStatLatLng,
-      destination: App.endPoint,
-      travelMode: google.maps.TravelMode.WALKING
-    };
-
-    App.directionsService.route(middleLeg, function(result, status) {
-      if (status == google.maps.DirectionsStatus.OK) {
-        $('#directions-info1').text("Walk to " + App.startStation.stationName);
-        $('#station-status1').text(App.startStation.availableBikes + " available bikes");
-        App.directionsDisplay2.setDirections(result);
-      }
-    });
-
-    App.directionsService.route(startLeg, function(result, status) {
-      if (status == google.maps.DirectionsStatus.OK) {
-        $('#directions-info2').text("Bike to " + App.endStation.stationName);
-        App.directionsDisplay1.setDirections(result);
-      }
-    });
-
-    App.directionsService.route(endLeg, function(result, status) {
-      if (status == google.maps.DirectionsStatus.OK) {
-        $('#directions-info3').text("Walk to " + App.endPoint);
-        $('#station-status3').text(App.endStation.availableDocks + " available docks");
-        App.directionsDisplay3.setDirections(result);
-      }
-    });
-  }
-}
 
 App.getDirections = function(){
   // get start and end
@@ -208,15 +79,15 @@ $(function(){
     e.preventDefault();
     App.getDirections();
 
+
+  // scroll to top of map on submit 
     $('html, body').animate({
         scrollTop: $("#map-canvas").offset().top
       }, 500);
-
-    // $('.directions-container').css({'display':'block'});
   });
 
+  // set start value to current location
   $('.input-group-addon').on('click', function(){
-    console.log("clicked")
     $('#start').val("Current Location") 
   })
 
